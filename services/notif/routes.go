6c07745a7946eb08cc4b92c67758c51d26968149
Amir <soltamir194@gmail.com>
2024-03-30 08:27:02 -0400

    upd

---
+++ services/notif/routes.go
@@ -0,0 +1,12 @@
+package notif
+
+import (
+	"basedpocket/base"
+
+	"github.com/labstack/echo/v4"
+	"github.com/pocketbase/pocketbase/core"
+)
+
+func handleCreateNotifs(app core.App, ctx echo.Context, env *base.Env) error {
+
+}
